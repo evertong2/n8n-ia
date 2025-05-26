3130ddc2720ae5fffaa2c2f35b7f132101413df4
evertong2 <47797349+evertong2@users.noreply.github.com>
2025-05-26 09:07:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-process.env.N8N_BASIC_AUTH_ACTIVE = process.env.N8N_BASIC_AUTH_ACTIVE || "true";
-process.env.N8N_BASIC_AUTH_USER = process.env.N8N_BASIC_AUTH_USER || "admin";
-process.env.N8N_BASIC_AUTH_PASSWORD = process.env.N8N_BASIC_AUTH_PASSWORD || "admin123";
-process.env.N8N_PORT = process.env.N8N_PORT || 10000;
-process.env.N8N_HOST = process.env.N8N_HOST || "0.0.0.0";
+const { exec } = require("child_process");
 
-const { start } = require("n8n");
-start();
+const env = {
+  ...process.env,
+  N8N_BASIC_AUTH_ACTIVE: process.env.N8N_BASIC_AUTH_ACTIVE || "true",
+  N8N_BASIC_AUTH_USER: process.env.N8N_BASIC_AUTH_USER || "admin",
+  N8N_BASIC_AUTH_PASSWORD: process.env.N8N_BASIC_AUTH_PASSWORD || "admin123",
+  N8N_HOST: "0.0.0.0",
+  N8N_PORT: "10000",
+  PORT: "10000"
+};
+
+exec("n8n start", { env }, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`[n8n error]`, error);
+    process.exit(1);
+  }
+  console.log(stdout);
+  console.error(stderr);
+});
